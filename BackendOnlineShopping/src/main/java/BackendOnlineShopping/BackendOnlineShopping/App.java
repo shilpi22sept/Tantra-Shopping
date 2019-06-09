cad8f04ae540ffb10e204811784018db00e8eb90
Shilpi <shilpisept22@gmail.com>
2019-06-09 17:40:21 +0530

    My Project setup completed successfully

---
+++ BackendOnlineShopping/src/main/java/BackendOnlineShopping/BackendOnlineShopping/App.java
@@ -0,0 +1,13 @@
+package BackendOnlineShopping.BackendOnlineShopping;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
